e11d671ef81baf782757c4f388a950abce103da0
sajim-khan <shossain1427@gmail.com>
2023-02-19 23:41:21 +0600

    added try catch finally

--- js/error.js
+++ js/error.js
@@ -1,11 +1,23 @@
+/*
 // 1. range error : not in js
 
 const numbers = [436,45,45,54,56]
 console.log(numbers[7]);    // it will show undefine
 
-// reference error 
+// 2. reference error 
 console.log(number);
 const number = 10
 
-// syntax error
+// 3. syntax error
+*/
+// Try Catch Finally
+try { console.log(djfhdj);
+    //const myName  = 'sajim';
+    
+} catch(error){
 
+    console.log(error);
+}
+finally{
+    console.log('this is the last line');
+}
