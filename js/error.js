6180487b4a6be9345c2cbbe60c1cb2b2b4bbb6ef
sajim-khan <shossain1427@gmail.com>
2023-02-19 22:30:28 +0600

    added some error

---
+++ js/error.js
@@ -0,0 +1,11 @@
+// 1. range error : not in js
+
+const numbers = [436,45,45,54,56]
+console.log(numbers[7]);    // it will show undefine
+
+// reference error 
+console.log(number);
+const number = 10
+
+// syntax error
+
