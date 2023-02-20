43c8b059b8f7a8e1cdf39877293a292f5327cc26
sajim-khan <shossain1427@gmail.com>
2023-02-21 01:17:02 +0600

    added spread syntax

--- js/spread.js
+++ js/spread.js
@@ -1,1 +1,15 @@
-const 
+const max = Math.max(3,43,45,56);
+console.log(max);
+
+const number = [3,43,45,56];
+const largest = Math.max(...number);
+//console.log(...number);
+//console.log(largest);
+
+
+const numbers = [3,43,45,56];
+const numbers2 = [67, 54, 54, ...numbers, 45,65];
+numbers.push(34)
+numbers2.push(42);
+console.log(numbers);
+console.log(numbers2);
